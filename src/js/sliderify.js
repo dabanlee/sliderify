3afe5237027a39b09ca9afca18a42dd395d1b01e
576839360@qq.com <576839360@qq.com>
2015-12-26 00:19:06 +0800

    Restructure the whole plugin
    - Makes the code easier to read

--- src/js/sliderify.js
+++ src/js/sliderify.js
@@ -1,203 +1,262 @@
 /*!
- * Sliderify - A simple slider.
- * http://justclear.github.io
+ * Sliderify - A Simple Slider.
+ * http://justclear.github.io/sliderify
  * 
  * Author: Clear
- * Version: 1.0.0
- * Date: 2015-12-24
+ * Version: 1.1.0
+ * Update: 2015-12-25
  * 
  * Released under the MIT license
  */
 
 ;(function($) {
 
-	var Sliderify = function(element, options) {
+	/**
+	 * Creates a slider.
+	 * @class The Sliderify.
+	 * @public
+	 * @param {HTMLElement|jQuery} element - The element to create the sliderify for.
+	 * @param {Object} [options] - The options
+	 */
+
+	function Sliderify(element, options) {
+
+		/**
+		 * Plugin element.
+		 * @public
+		 */
 		this.element = element;
-		this.opts = $.extend({}, $.fn.sliderify.defaults, options);
-		this.init();
+		this.$element = $(element);
+
+		/**
+		 * Current options set by the caller including defaults.
+		 * @public
+		 */
+		this.options = $.extend({}, $.fn.sliderify.defaults, options);
+		this.initialize();
 	}
 
-	Sliderify.prototype = {
+	/**
+	 * Initializes the sliderify.
+	 * @protected
+	 */
+	Sliderify.prototype.initialize = function() {
+		var element = this.element,
+			currentIndex = this.options.initialSlide;
 
-		// initial
-		init: function() {
-			var self = this,
-				element = this.element,
-				$this = $(this.element),
-				opts = this.opts;
+		// console.log(this.element);	
 
-			// console.log(opts);
+		this.render();
+		this.setup();
+		this.setStyle();
+	}
 
-			self.render(element);
-			self.main(element);
-			self.setStyle(element);
-			self.setAnimation();
-		},
+	/**
+	 * Render the sliderify.
+	 * @public
+	 */
+	Sliderify.prototype.render = function() {
+		//
+		// console.log('render');
+		// console.log(this.slideNum());
+		var sliderNum = this.slideNum(),
+			currentIndex = $(this).index(),
+			bulletText = '';
 
-		// Main
-		main: function(element) {
-			//
-			var $this = $(element),
-				self = this,
-				currentIndex = 0;
+		// render bullet
+		for(var i = 0; i < sliderNum; i ++) {
+			bulletText += '<span class="sliderify-bullet"></span>';
+		}
 
-			$this.find('.sliderify-bullet').on('click', function() {
+		$(bulletText).appendTo('.sliderify-pagination');
 
-				$(this).addClass('current')
-				.siblings()
-				.removeClass('current');
+		if((this.options.initialSlide - 1) < 0) {
 
-				currentIndex = $(this).index();
-				changeSlider(currentIndex);
-			});
+			this.changeSlider(0);
 
-			$this.find('.sliderify-prev').on('click', function() {
+		} else if((this.options.initialSlide - 1) > sliderNum){
 
-				prevSlider();
-			});
+			this.changeSlider(sliderNum - 1);
 
-			$this.find('.sliderify-next').on('click', function() {
+		} else {
 
-				nextSlider();
-			});
+			this.changeSlider(this.options.initialSlide - 1);
 
-			if(this.opts.keyboardControl === true) {
-
-				$(document).on('keydown', self.keyEvents);
-			}
-
-			if(this.opts.autoPlay === true) {
-
-				this.autoPlay();
-			}
-		},
-
-		render: function(element) {
-			//
-			var self = this,
- 				$this = $(element),
-				sliderItems = $this.find('.sliderify-slide'),
-				sliderNum = sliderItems.size(),
-				currentIndex = $(this).index(),
-				bulletText = '';
-
-			// render bullet
-			for(var i = 0; i < sliderNum; i ++) {
-				bulletText += '<span class="sliderify-bullet"></span>';
-			}
-
-			$(bulletText).appendTo('.sliderify-pagination');
-
-			if((this.opts.initialSlide - 1) < 0) {
-
-				changeSlider(0);
-
-			} else if((this.opts.initialSlide - 1) > sliderNum){
-
-				changeSlider(sliderNum - 1);
-
-			} else {
-
-				changeSlider(this.opts.initialSlide - 1);
-
-			}
-		},
-
-		// Set Style
-		setStyle: function(element) {
-			var $this = $(element);
-
-			sliderItems = $this.find('.sliderify-slide');
-			sliderNum = sliderItems.size();
-
-			$('.sliderify-container').css({
-				// 
-			});
-
-			sliderItems.css({
-				'width' : (100 / sliderNum) + '%'
-			});
-
-			$('.sliderify-container').children('.sliderify-wrapper').css({
-				'width': sliderNum * 100 + '%'
-			});
-
-			if(this.opts.hidePaginationButtons === true) {
-
-				$this.find('.sliderify-prev, .sliderify-next').css({
-					'display': 'none'
-				});
-			}
-
-			if(this.opts.hidePagination === true) {
-
-				$this.find('.sliderify-pagination').css({
-					'display': 'none'
-				});
-			}
-		},
-
-		// Set Animation
-		setAnimation: function() {
-
-			$('.sliderify-container').animate({
-				// 
-			});
-		},
-
-		// Key Events
-		keyEvents: function(event) {
-
-			var keyCode = event.keyCode || event.which,
-				self = this;
-
-			// keyCode 65 => A, keyCode 37 => Left
-			if(keyCode === 65 || keyCode === 37) {
-
-				prevSlider();
-			}
-
-			// keyCode 68 => S, keyCode 39 => Right
-			if(keyCode === 68 || keyCode === 39) {
-
-				nextSlider();
-			}
-		},
-
-		// Auto Play
-		autoPlay: function() {
-
-			var timer;
-
-			timer = setInterval(function() {
-				
-				nextSlider();
-			}, this.opts.autoPlayInterval);
 		}
 	}
 
-	function changeSlider(currentIndex) {
+	/**
+	 * Setup the plugin.
+	 * @protected
+	 */
+	Sliderify.prototype.setup = function() {
 		//
+		// console.log('setup');
+		var currentIndex = 0,
+			self = this;
+
+		this.$element.find('.sliderify-bullet').on('click', function() {
+
+			$(this).addClass('current')
+			.siblings()
+			.removeClass('current');
+
+			currentIndex = $(this).index();
+			self.changeSlider(currentIndex);
+		});
+
+		this.$element.find('.sliderify-prev').on('click', function() {
+
+			self.prevSlider();
+		});
+
+		this.$element.find('.sliderify-next').on('click', function() {
+
+			self.nextSlider();
+		});
+
+		if(this.options.keyboardControl === true) {
+
+			$(document).on('keydown', this.keyEvents);
+		}
+
+		if(this.options.autoPlay === true) {
+
+			this.autoPlay();
+		}
+	}
+
+	/**
+	 * Set style.
+	 * @public
+	 */
+	Sliderify.prototype.setStyle = function() {
+		//
+		// console.log('setStyle');
+
+		var sliderNum = this.slideNum();
+
+		$('.sliderify-container').css({
+			// 
+		});
+
+		this.$element.find('.sliderify-slide').css({
+			'width' : (100 / sliderNum) + '%'
+		});
+
+		$('.sliderify-container').children('.sliderify-wrapper').css({
+			'width': sliderNum * 100 + '%'
+		});
+
+		if(this.options.hidePaginationButtons === true) {
+
+			this.$element.find('.sliderify-prev, .sliderify-next').css({
+				'display': 'none'
+			});
+		}
+
+		if(this.options.hidePagination === true) {
+
+			this.$element.find('.sliderify-pagination').css({
+				'display': 'none'
+			});
+		}
+	}
+
+	/**
+	 * Set animation.
+	 * @public
+	 */
+	Sliderify.prototype.setAnimation = function() {
+		//
+		// console.log('setStyle');
+
+	}
+
+	/**
+	 * The keyboard events.
+	 * @public
+	 */
+	Sliderify.prototype.keyEvents = function() {
+		//
+		console.log('keyEvents');
+
+		var keyCode = event.keyCode || event.which,
+			self = this;
+
+		// keyCode 65 => A, keyCode 37 => Left
+		if(keyCode === 65 || keyCode === 37) {
+
+			self.prevSlider();
+		}
+
+		// keyCode 68 => S, keyCode 39 => Right
+		if(keyCode === 68 || keyCode === 39) {
+
+			self.nextSlider();
+		}
+	}
+
+	/**
+	 * Auto play function.
+	 * @public
+	 */
+	Sliderify.prototype.autoPlay = function() {
+		//
+		// console.log('autoPlay');
+
+		var self = this,
+			timer = null,
+			autoPlayInterval = null,
+			slidingInterval = null,
+			interval = null;
+
+		/**
+		 * Make sure auto play interval greater than sliding interval
+		 */
+		autoPlayInterval = this.options.autoPlayInterval,
+		slidingInterval = this.options.slidingInterval,
+		interval = autoPlayInterval > slidingInterval ? autoPlayInterval : slidingInterval;
+
+		timer = setInterval(function() {
+			
+			self.nextSlider();
+		}, interval);
+	}
+
+	/**
+	 * Slides to the specified item.
+	 * @public
+	 * @param {Number} index - The index of the item.
+	 */
+	Sliderify.prototype.changeSlider = function(currentIndex) {
+		//
+		// console.log('changeSlider');
+
 		var currentIndex = currentIndex,
 			pagination = $('.sliderify-pagination');
 
-		$('.sliderify-slide').removeClass('current')
+		this.$element.find('.sliderify-slide').removeClass('current')
 		.eq(currentIndex).addClass('current');
 
 		pagination.children('span').removeClass('current')
 		.eq(currentIndex).addClass('current');
 
-		$('.sliderify-wrapper').animate({
+		this.$element.find('.sliderify-wrapper').animate({
 			'left': (- currentIndex * 100) + '%'
-		});
-
+		}, this.options.slidingInterval, this.options.slidingEasing);
 	}
 
-	function prevSlider() {
+	/**
+	 * Slides to the previous item.
+	 * @public
+	 */
+	Sliderify.prototype.prevSlider = function() {
+		//
+		console.log('prevSlider');
 
-		var slideNum = $('.sliderify-slide').size(),
-			currentIndex = $('.current').index(),
-			self = this;
+		var slideNum = this.slideNum(),
+			currentIndex = $('.current').index();
 
 		currentIndex --;
 
@@ -206,16 +265,19 @@
 			currentIndex = slideNum - 1;
 		}
 
-		changeSlider(currentIndex);
-
-		// console.log('Prev');
+		this.changeSlider(currentIndex);
 	}
 
-	function nextSlider() {
+	/**
+	 * Slides to the next item.
+	 * @public
+	 */
+	Sliderify.prototype.nextSlider = function() {
+		//
+		console.log('nextSlider');
 
-		var slideNum = $('.sliderify-slide').size(),
-			currentIndex = $('.current').index(),
-			self = this;
+		var slideNum = this.slideNum(),
+			currentIndex = $('.current').index();
 
 		currentIndex ++;
 
@@ -224,9 +286,20 @@
 			currentIndex = 0;
 		}
 
-		changeSlider(currentIndex);
+		this.changeSlider(currentIndex);
+	}
 
-		// console.log('Next');
+	/**
+	 * Slider items number.
+	 * @public
+	 * @return {Number} - the number of slides
+	 */
+	Sliderify.prototype.slideNum = function() {
+		//
+		// console.log('sliderItems');
+		var slideNum = this.$element.find('.sliderify-slide').size();
+
+		return slideNum;
 	}
 
 	$.fn.sliderify = function(options) {
@@ -237,10 +310,16 @@
 		});
 	};
 
+	/**
+	 * Sliderify default options.
+	 * @public
+	 */
 	$.fn.sliderify.defaults = {
 		initialSlide: 0,
 		autoPlay: true,
 		autoPlayInterval: 3000,
+		slidingInterval: 5000,
+		slidingEasing: 'linear',
 		hidePagination: false,
 		hidePaginationButtons: false,
 		keyboardControl: true
